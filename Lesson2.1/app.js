325bc7793ba792232cfa9299bda761a21858f02e
Irina Ponomareva <irina@Irinas-MacBook-Pro.local>
2025-04-12 19:56:09 +0200

    Обновила переменные в Lesson2.2

---
+++ Lesson2.1/app.js
@@ -0,0 +1,6 @@
+const firstName = 'Irina'
+console.log('Hello', firstName)
+const mycity = 'Lugano'
+console.log('I live in', mycity)
+const hobby = 'Webflow'
+console. log('I adore', hobby)
--- app.js
+++
@@ -1,4 +0,0 @@
-const firstName = 'Irina'
-console.log('Hello', firstName)
-const mycity = 'Lugano'
-console.log('I live', mycity)
