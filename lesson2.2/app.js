7f65481d794708eba584eff2374de13cbe55d819
Irina Ponomareva <irina@Irinas-MacBook-Pro.local>
2025-04-12 21:05:31 +0200

    Заново добавила lesson2.2 как обычную папку

---
+++ lesson2.2/app.js
@@ -0,0 +1,7 @@
+const titleProject = "Оценка стоимости";
+const screensValue = "шаблонные, с уникальным дизайном, с анимациями";
+const screenPrice = 5000;
+const percentage = 10;
+const fullPrice = 150000;
+const responsive = true;
+console.log(titleProject, screensValue, screenPrice, percentage, fullPrice, responsive);
